4f9e51a0fb7a35d6822c1ca2582967f654aa9f21
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-06-24 17:56:52 +0100

    Add 3 functions to allow time to be printed to console

--- DisplayTime.cs
+++ DisplayTime.cs
@@ -6,5 +6,24 @@ namespace DigitalClock
 {
     class DisplayTime
     {
+
+        String time;
+        public void RefreshTime()
+        {
+            TimeToString();
+            Console.Clear();
+            Console.WriteLine(time);
+            Delay(1);
+        }
+
+        private void TimeToString()
+        {
+            time = DateTime.Now.ToString("h:mm:ss");
+        }
+
+        private void Delay(int seconds)
+        {
+            System.Threading.Thread.Sleep(seconds * 1000);
+        }
     }
 }
