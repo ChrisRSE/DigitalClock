1261c046cd7fb992f15c35408725663a84d57079
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-06-25 07:53:25 +0100

    Revert "Add 3 functions to allow time to be printed to console"
    This reverts commit 4f9e51a0fb7a35d6822c1ca2582967f654aa9f21.

--- DisplayTime.cs
+++ DisplayTime.cs
@@ -6,24 +6,5 @@ namespace DigitalClock
 {
     class DisplayTime
     {
-
-        String time;
-        public void RefreshTime()
-        {
-            TimeToString();
-            Console.Clear();
-            Console.WriteLine(time);
-            Delay(1);
-        }
-
-        private void TimeToString()
-        {
-            time = DateTime.Now.ToString("h:mm:ss");
-        }
-
-        private void Delay(int seconds)
-        {
-            System.Threading.Thread.Sleep(seconds * 1000);
-        }
     }
 }
