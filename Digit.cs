4535fbc4a69171e3a69d23b6bf547518732d0ef1
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-06-25 18:51:13 +0100

    Add Digit Class

---
+++ Digit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitalClock
+{
+    class Digit
+    {
+        readonly char[,] _layout;
+
+        public Digit(int height, int width)
+        {
+            _layout = new char[height, width];
+        }
+    }
+}
