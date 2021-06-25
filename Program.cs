561fe917ebe97dd84e7df53e6fd5f7f48379b023
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-06-25 07:52:41 +0100

    Add Face Class

--- Program.cs
+++ Program.cs
@@ -7,7 +7,38 @@ namespace DigitalClock
     {
         static void Main(string[] args)
         {
+            DisplayTime displayTime = new DisplayTime();
 
+            //Console.WriteLine("19:40:21");
+            //Console.WriteLine("19:40:22");
+            //Console.WriteLine("19:40:23");
+
+            /*for (int i = 0; i < 60; i++)
+            {
+                displayTime.RefreshTime();
+            }*/
+
+            char[] myArray = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '=' };
+
+            Console.WriteLine("Test");
+
+            //first number
+            int i = 0;
+            int j = 6;
+            myArray[i+1] = '#';
+            myArray[i+2] = '#';
+            myArray[i+3] = '#';
+            myArray[i+4] = '#';
+
+            myArray[j + 1] = '#';
+            myArray[j + 2] = '#';
+            myArray[j + 3] = '#';
+            myArray[j + 4] = '#';
+
+            foreach (var letter in myArray)
+            {
+                Console.Write(letter);
+            }
         }
     }
 }
