2660e1072fbdbcd00a8851a1b90c18753dae9b7b
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-06-26 18:29:15 +0100

    Added methods for displaying time more effectively

--- DisplayTime.cs
+++ DisplayTime.cs
@@ -6,34 +6,50 @@ namespace DigitalClock
 {
     class DisplayTime
     {
-        String time;
-        Face face = new Face();
-        DigitList digitList = new DigitList();
+        private char[] time;
+        private int[] hour = new int[2];
+        private int[] min = new int[2];
+        private int[] sec = new int[2];
+        private Face face = new Face();
+        private DigitList digitList = new DigitList();
 
         public void RefreshTime()
         {
-            TimeToString();
             Console.Clear();
-            Console.WriteLine(time);
-            Delay(1);
-        }
-
-        private void TimeToString()
-        {
-            time = DateTime.Now.ToString("h:mm:ss");
-        }
-
-        private void Delay(int seconds)
-        {
-            System.Threading.Thread.Sleep(seconds * 1000);
-        }
-
-        public void PrintTest()
-        {
+            face.ResetBoard();
+            TimeToInt();
+            SetTime();
             face.PrintBoard();
+            Delay(1000);
         }
 
-        public void UpdateTime(Digit digit, int position)
+        public void TimeToInt()
+        {
+            time = DateTime.Now.ToLongTimeString().ToCharArray();
+            hour[0] = time[0] - '0';
+            hour[1] = time[1] - '0';
+            min[0] = time[3] - '0';
+            min[1] = time[4] - '0';
+            sec[0] = time[6] - '0';
+            sec[1] = time[7] - '0';
+        }
+
+        private void Delay(int milliseconds)
+        {
+            System.Threading.Thread.Sleep(milliseconds);
+        }
+
+        private void SetTime()
+        {
+            UpdateTime(digitList.Digits[hour[0]], 1);
+            UpdateTime(digitList.Digits[hour[1]], 2);
+            UpdateTime(digitList.Digits[min[0]], 3);
+            UpdateTime(digitList.Digits[min[1]], 4);
+            UpdateTime(digitList.Digits[sec[0]], 5);
+            UpdateTime(digitList.Digits[sec[1]], 6);
+        }
+
+        private void UpdateTime(Digit digit, int position)
         {
             int offset = 0;
             for(int i = 0; i < digitList.Height; i++)
