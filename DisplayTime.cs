29be90e1128e135d79738b77423dc4d5aaf06ad3
ChrisRSE <84640547+ChrisRSE@users.noreply.github.com>
2021-06-26 17:14:09 +0100

    Added update time function

--- DisplayTime.cs
+++ DisplayTime.cs
@@ -7,6 +7,9 @@ namespace DigitalClock
     class DisplayTime
     {
         String time;
+        Face face = new Face();
+        DigitList digitList = new DigitList();
+
         public void RefreshTime()
         {
             TimeToString();
@@ -23,6 +26,50 @@ namespace DigitalClock
         private void Delay(int seconds)
         {
             System.Threading.Thread.Sleep(seconds * 1000);
+        }
+
+        public void PrintTest()
+        {
+            face.PrintBoard();
+        }
+
+        public void UpdateTime(Digit digit, int position)
+        {
+            int offset = 0;
+            for(int i = 0; i < digitList.Height; i++)
+            {
+                for(int j = 0; j < digitList.Width; j++)
+                {
+                    if(digit.Layout[i,j] == '#')
+                    {
+                        if (position == 1)
+                        {
+                            offset = 3;
+                        }
+                        if (position == 2)
+                        {
+                            offset = 11;
+                        }
+                        if (position == 3)
+                        {
+                            offset = 22;
+                        }
+                        if (position == 4)
+                        {
+                            offset = 30;
+                        }
+                        if (position == 5)
+                        {
+                            offset = 41;
+                        }
+                        if (position == 6)
+                        {
+                            offset = 49;
+                        }
+                        face.ChangeChar(i, j, offset);
+                    }
+                }
+            }
         }
     }
 }
